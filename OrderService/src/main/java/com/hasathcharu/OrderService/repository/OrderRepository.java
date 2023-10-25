3e778744184cb4b10a46af1a42d11f496dfa5876
Haritha Hasathcharu <harithahasathcharu@icloud.com>
2023-10-25 17:05:44 +0530

    remove files

--- OrderService/src/main/java/com/hasathcharu/OrderService/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hasathcharu.OrderService.repository;
-
-import com.hasathcharu.OrderService.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- ProductService/src/main/java/com/hasathcharu/ProductService/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hasathcharu.ProductService.repository;
-
-import com.hasathcharu.ProductService.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
