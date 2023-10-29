dc1ef6e235a1189c62a64082738c9f52ef3c94eb
RashmiSandamini <rashmi20sandamini@gmail.com>
2023-10-29 23:35:24 +0530

    generated jwt

---
+++ UserManagement/src/main/java/org/ead/usermanagement/model/Role.java
@@ -0,0 +1,6 @@
+package org.ead.usermanagement.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- UserManagement/src/main/java/org/ead/usermanagement/model/User.java
+++ UserManagement/src/main/java/org/ead/usermanagement/model/User.java
@@ -1,5 +1,7 @@
 package org.ead.usermanagement.model;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,13 +10,19 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Document(value = "user")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     private ObjectId id;
     private String name;
@@ -23,4 +31,42 @@ public class User {
     private String gender;
     private String telephone;
     private String address;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
